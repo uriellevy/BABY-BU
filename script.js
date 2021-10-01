f41182fe1dd8b67ca50c918d580d627bf1c0b9c5
uriel levy <uriellevy4892@gmail.com>
2021-10-01 10:50:48 +0300

    gellery added

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-////////HEADER RESPONSIVE NAVBAR
+////////HEADER RESPONSIVE NAVBAR///////
 
 const links = document.querySelectorAll("nav ul li a");
 const icon = document.getElementById("icon");
@@ -27,3 +27,31 @@ const nav = document.querySelector("nav");
 window.addEventListener("scroll", function () {
   nav.classList.toggle("sticky", window.scrollY > 400);
 });
+
+///////////////////GALLERY////////////////
+const current = document.querySelector("#current");
+const imgs = document.querySelectorAll(".imgs img");
+const opacity = 0.6;
+
+//set first img opacity
+imgs[0].style.opacity = opacity;
+
+imgs.forEach((img) => {
+  img.addEventListener("click", imgClick);
+});
+
+function imgClick(e) {
+  //reset all imgs opacity
+  imgs.forEach((img) => {
+    img.style.opacity = 1;
+  });
+  //change img to src img
+  current.src = e.target.src;
+
+  //add fadeIn class
+  current.classList.add("fade-in");
+  //remove fadein class after 0.5s
+  setTimeout(() => current.classList.remove("fade-in"), 500);
+  //change opacity
+  e.target.style.opacity = opacity;
+}
